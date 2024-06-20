b7434a450de856407f5ed24551a6c828ab0221c9
huda elhady <huda.m.elhady@gmail.com>
2024-06-20 23:38:07 +0300

    add new project

---
+++ environmentObject-example/environmentObject-example/environmentObject_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  environmentObject_exampleApp.swift
+//  environmentObject-example
+//
+//  Created by huda elhady on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct environmentObject_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
